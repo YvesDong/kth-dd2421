64706228d6d381c541cf9ab9e77bbf98419d5946
Yifei DONG <yifei.dong@idiap.ch>
2023-01-29 13:30:45 +0100

    assignment 6 added

--- dectrees/python/main.py
+++ dectrees/python/main.py
@@ -35,7 +35,7 @@ for i in range (0,NumAttributes):
 splitAttri_l0 = np.argmax(G, axis=1)
 
 print("\nASSIGNMENT 3: ")
-print("The information gain matrix: \n", G)
+print("The information gain matrix: \n", G, "\n")
 for i in range(3):
     print("For dataset monk{}, node 0 in Layer 0 can be splitted by attribute {}.".format(i+1, m.attributes[splitAttri_l0[i]].name))
 
@@ -43,45 +43,44 @@ for i in range(3):
 # not sure if this is needed for the presentation
 # not sure if it is correct either
 
-# split dataset1 into two parts
-monk11 = fun.select(m.monk1, m.attributes[4], 1)
-monk12 = fun.select(m.monk1, m.attributes[4], 2)
-monk12 = monk12 + fun.select(m.monk1, m.attributes[4], 3)
-monk12 = monk12 + fun.select(m.monk1, m.attributes[4], 4)
+# # split dataset1 into two parts
+# monk11 = fun.select(m.monk1, m.attributes[4], 1)
+# monk12 = fun.select(m.monk1, m.attributes[4], 2)
+# monk12 = monk12 + fun.select(m.monk1, m.attributes[4], 3)
+# monk12 = monk12 + fun.select(m.monk1, m.attributes[4], 4)
 
-#compute information gain on the nodes
-G_monk1 = np.zeros((2,6))
+# #compute information gain on the nodes
+# G_monk1 = np.zeros((2,6))
 
-for i in range (0,6):
-    G_monk1[0,i] = fun.averageGain(monk11, m.attributes[i])
-    G_monk1[1,i] = fun.averageGain(monk12, m.attributes[i])
+# for i in range (0,6):
+#     G_monk1[0,i] = fun.averageGain(monk11, m.attributes[i])
+#     G_monk1[1,i] = fun.averageGain(monk12, m.attributes[i])
 
-print(G_monk1)
+# print(G_monk1)
 
-# # split dataset1 into 4 parts based on attribute 5
-# splitAttri_l0_monk1 = splitAttri_l0[0]
-# print(splitAttri_l0_monk1)
-# NumValAttr5 = len(m.attributes[splitAttri_l0_monk1].values)
-# monk1_l1 = []
-# for i in range(NumValAttr5):
-#     monk1_l1.append(fun.select(m.monk1, m.attributes[4], m.attributes[4].values[i]))
+# split dataset1 into 4 parts based on attribute 5
+splitAttri_l0_monk1 = splitAttri_l0[0]
+NumValAttr5 = len(m.attributes[splitAttri_l0_monk1].values)
+monk1_l1 = []
+for i in range(NumValAttr5):
+    monk1_l1.append(fun.select(m.monk1, m.attributes[4], m.attributes[4].values[i]))
 
-# # compute information gain on the nodes
-# G_monk_l1 = np.zeros((NumValAttr5, NumAttributes))
-# for i in range(NumAttributes):
-#     for j in range(NumValAttr5):
-#         G_monk_l1[j,i] = fun.averageGain(monk1_l1[j], m.attributes[i])
+# compute information gain on the nodes
+G_monk_l1 = np.zeros((NumValAttr5, NumAttributes))
+for i in range(NumAttributes):
+    for j in range(NumValAttr5):
+        G_monk_l1[j,i] = fun.averageGain(monk1_l1[j], m.attributes[i])
 
-# # attribute for splitting
-# # G_monk1[0,i] == 0 means no need to split again
-# print("Node 0 in Layer 1 has no leaf node. Its majority class is ", fun.mostCommon(monk1_l1[0]))
+# attribute for splitting
+# G_monk1[0,i] == 0 means no need to split again
+print("Node 0 in Layer 1 has no leaf node. Its majority class is ", fun.mostCommon(monk1_l1[0]))
 
-# # print(G_monk_l1)
-# splitAttri_l1 = np.argmax(G_monk_l1[1:], axis=1)
-# for i in range(NumValAttr5-1):
-#     print("Node {} in Layer 1 can be splitted by attribute {}. Its majority class is {}".format(i+1, m.attributes[splitAttri_l1[i]].name, fun.mostCommon(monk1_l1[i+1])))
+# print(G_monk_l1)
+splitAttri_l1 = np.argmax(G_monk_l1[1:], axis=1)
+for i in range(NumValAttr5-1):
+    print("Node {} in Layer 1 can be splitted by attribute {}. Its majority class is {}".format(i+1, m.attributes[splitAttri_l1[i]].name, fun.mostCommon(monk1_l1[i+1])))
 
-# # build and draw tree with predefined function
+# build and draw tree with predefined function
 # draw.drawTree(fun.buildTree(m.monk1, m.attributes, 2))
 
 
@@ -100,6 +99,13 @@ print("Monk-2, E_test: ", 1-fun.check(tree2, m.monk2test))
 print("Monk-3, E_train: ", 1-fun.check(tree3, m.monk3))
 print("Monk-3, E_test: ", 1-fun.check(tree3, m.monk3test))
 
+##### ASSIGNMENT 6 ###########################################################
+print("\nASSIGNMENT 6: \nExplain pruning from a bias variance trade-off perspective.")
+print("The bias–variance tradeoff is the property of a model that the variance of the parameter",
+    "estimated across samples can be reduced by increasing the bias in the estimated parameters.",
+    "Tree pruning basically means avoiding a high variance and overfitting of the model,",
+    "at the cost of increasing the bias a bit. The overall error of prediction should be",
+    " in this case be reduced.")
 
 ##### ASSIGNMENT 7 ###########################################################
 fractions = [.3, .4, .5, .6, .7, .8]
