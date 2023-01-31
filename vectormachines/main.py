12d25c76e7709ba877938b3a593ddd87142c6f51
kmatterjo <123721585+kmatterjo@users.noreply.github.com>
2023-01-31 13:48:11 +0100

    Added new directory for assignment2

---
+++ vectormachines/main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import random , math
+import matplotlib.pyplot as plt
+from scipy.optimize import minimize
+
+
