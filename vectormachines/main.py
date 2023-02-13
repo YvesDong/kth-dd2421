c39c74b9f455ea8f7ca71f33dc040432532b3024
Yifei DONG <yifei.dong@idiap.ch>
2023-02-13 18:53:53 +0100

    subplots added

--- vectormachines/main.py
+++ vectormachines/main.py
@@ -4,82 +4,129 @@ import random, math
 import matplotlib.pyplot as plt
 from scipy.optimize import minimize
 
-def zerofun(alpha):
-    return np.dot(alpha,targets)
-
 # GENERATE DATAPOINTS ###############################################################################
+def generate_data(Num, pos1=[-1.5, 0.5]):
+    # Num = 20 # number of data points
+    # np.random.seed(100)
+    classA = np.concatenate((np.random.randn(int(Num/2), 2)*0.2 + [1.5, 0.5], np.random.randn(int(Num/2), 2)*0.2 + pos1))
+    classB = np.random.randn(Num, 2)*0.2 + [0.0, -0.5]
 
-Num = 20 # number of data points
-# ynp.random.seed(100)
-classA = np.concatenate((np.random.randn(int(Num/2), 2)*0.2 + [1.5, 0.5], np.random.randn(int(Num/2), 2)*0.2 + [-1.5, 0.5]))
-classB = np.random.randn(Num, 2)*0.2 + [0.0, -0.5]
+    inputs = np.concatenate((classA, classB))
+    targets = np.concatenate((np.ones(classA.shape[0]), -np.ones(classB.shape[0])))
 
-inputs = np.concatenate((classA, classB))
-targets = np.concatenate((np.ones(classA.shape[0]), -np.ones(classB.shape[0])))
+    N = inputs.shape[0] # Number of rows (samples)
 
-N = inputs.shape[0] # Number of rows (samples)
+    permute = list(range(N))
+    random.shuffle(permute)
+    inputs = inputs[permute,:]
+    targets = targets[permute]
 
-permute = list(range(N))
-random.shuffle(permute)
-inputs = inputs[permute,:]
-targets = targets[permute]
+    return inputs, targets, classA, classB
 
 # PLOT GENERATED DATA ##############################################################################
 
-plt.plot([p[0] for p in classA], [p[1] for p in classA], 'b.')
-plt.plot([p[0] for p in classB], [p[1] for p in classB], 'r.')
+# plt.plot([p[0] for p in classA], [p[1] for p in classA], 'b.')
+# plt.plot([p[0] for p in classB], [p[1] for p in classB], 'r.')
 
-plt.axis('equal')
-plt.show()
+# plt.axis('equal')
+# plt.show()
 # plt.savefig('svmplot.pdf')
 
 # OPTIMIZATION #####################################################################################
 
-# kernel = "linKernel"
-kernel = "polyKernel"
-# kernel = "RBFKernel"
+def training(kernel="linKernel", p=2, sigma=1):
+    # kernel = "linKernel"
+    # kernel = "polyKernel"
+    # kernel = "RBFKernel"
 
-# nonlinear kernel parameters
-p = 2 
-sigma = 1
+    # nonlinear kernel parameters
+    # p = 2 
+    # sigma = 1
 
-fun.KernelMatrix(N, targets, inputs, kernel, sigma, p)  
+    def zerofun(alpha):
+        return np.dot(alpha, targets)
 
-C = None # slack variable
-start = np.zeros(N)
-B = [(0, C) for b in range(N)]
-XC = {'type':'eq', 'fun':zerofun}
+    fun.KernelMatrix(2*Num, targets, inputs, kernel, sigma, p)  
 
-ret = minimize(fun.objective, start, bounds=B, constraints=XC)
+    # C = None # slack variable
+    start = np.zeros(2*Num)
+    B = [(0, C) for b in range(2*Num)]
+    XC = {'type':'eq', 'fun':zerofun}
 
-if (ret['success'] == True):
-    print("Optimization successful!")
-else:
-    print("Optimization failed!")
-    exit()
+    ret = minimize(fun.objective, start, bounds=B, constraints=XC)
 
-# extract support vectors and calculate threshold 
-alpha = ret['x']
-supportvectors = []
+    if (ret['success'] == True):
+        print("Optimization successful!")
+    else:
+        print("Optimization failed!")
+        exit()
+    alpha = ret['x']
 
-for i in range(N):
-    if (alpha[i] > pow(10,-5)):
-        # print(alpha[i], inputs[i], targets[i])
-        supportvectors.append([alpha[i], inputs[i], targets[i]])
+    return alpha
 
-b = fun.threshold(supportvectors, C, kernel, sigma, p)
-print("threshold b = ", b)
+# extract support vectors and calculate threshold ##########################################################
+def extractSV(C=None, kernel="linKernel", p=2, sigma=1):
+    supportvectors = []
+
+    for i in range(2*Num):
+        if (alpha[i] > pow(10,-5)):
+            # print(alpha[i], inputs[i], targets[i])
+            supportvectors.append([alpha[i], inputs[i], targets[i]])
+    print("number of non-zero alpha: ", len(supportvectors))
+
+    b = fun.threshold(supportvectors, C, kernel, sigma, p)
+    print("threshold b = ", b)
+
+    return supportvectors, b
 
 # PLOT DECISION BOUNDARIES AND DATAPOINTS ##########################################################
+def plot_results(axs, kernel="linKernel", p=2, sigma=1):
+    xgrid = np.linspace(-3, 3)
+    ygrid = np.linspace(-3, 3)
+    grid = np.array([[fun.indicator(x, y, supportvectors, b, kernel, sigma, p) for x in xgrid] for y in ygrid])
 
-xgrid = np.linspace(-5, 5)
-ygrid = np.linspace(-4, 4)
+    axs.contour(xgrid, ygrid, grid, (-1.0,0.0,1.0), colors=('red','black','blue'), linewidths=(1,3,1))
+    axs.plot([p[0] for p in classA], [p[1] for p in classA], 'b.')
+    axs.plot([p[0] for p in classB], [p[1] for p in classB], 'r.')
+    axs.set_title("# sigma RBFK: {}, % pos alpha: {}".format(sigma, "%.2f" % (len(supportvectors)/(2*Num))))
+    # axs.set_title("# data: {}, % pos alpha: {}".format(2*Num, "%.2f" % (len(supportvectors)/(2*Num))))
+    # plt.axis('equal')
 
-grid = np.array([[fun.indicator(x, y, supportvectors, b, kernel, sigma, p) for x in xgrid] for y in ygrid])
 
-plt.contour(xgrid, ygrid, grid, (-1.0,0.0,1.0), colors=('red','black','blue'), linewidths=(1,3,1))
-plt.plot([p[0] for p in classA], [p[1] for p in classA], 'b.')
-plt.plot([p[0] for p in classB], [p[1] for p in classB], 'r.')
+if __name__ == "__main__":
+    # hyperparameters
+    Num = 100 # no of data for each of the class
+    C = 10 # slack variable
+    kList = ['linKernel', 'polyKernel', 'RBFKernel']
+    kernel = kList[2]
+    p = 2
+    sigma = 1
+    pos1 = [-1.5, 0.5]
 
-plt.axis('equal')
-plt.show()
+    # inputs, targets, classA, classB = generate_data(Num)
+    # alpha = training(kernel=kernel, p=p, sigma=sigma)
+    # supportvectors, b = extractSV(C=C, kernel=kernel, p=p, sigma=sigma)
+    # plot_results(kernel=kernel, p=p, sigma=sigma)
+
+    ## exploring and reporting
+    # numList = [20, 80, 160, 320] # discussion 1 - change Num
+    # pos1List = [[-1.5, 0.5], [-.5, 0.4], [.5, 0.3], [-1, 0.2]] # discussion 1 - change cluster position
+    pList = [1,2,3,4] # discusstion 3 - p in polyKernel
+    sigmaList = [.5,.7,2,5] # discusstion 3 - p in polyKernel
+
+    inputs, targets, classA, classB = generate_data(Num, pos1)
+    fig, axs = plt.subplots(2, 2) # plots
+    for i in range(4):
+        # Num = numList[i]
+        # pos1 = pos1List[i]
+        # p = pList[i]
+        sigma = sigmaList[i]
+
+        alpha = training(kernel=kernel, p=p, sigma=sigma)
+        supportvectors, b = extractSV(C=C, kernel=kernel, p=p, sigma=sigma)
+        
+        idx1, idx2 = [0,0,1,1], [0,1,0,1]
+        plot_results(axs[idx1[i], idx2[i]], kernel=kernel, p=p, sigma=sigma)
+    fig.tight_layout()
+    plt.show()
+
